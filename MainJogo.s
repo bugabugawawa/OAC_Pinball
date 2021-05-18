9e838bdbdcd64f5fb90a49087a3c5685279029be
bugabugawawa <gabriel.ligoski@gmail.com>
2021-05-18 16:58:01 -0300

    fixing things

--- MainJogo.s
+++ MainJogo.s
@@ -14,7 +14,11 @@ Inicio:
 	li a7 10
 	ecall
 
+
+# Declaração de funções necessárias
 subir: upSelected(s2)
 descer: downSelected(s2)
+N2IP1: num2img( s3 t4 )
+N2IP2: num2img( s4 t4 )
 
 
--- testLigoski.s
+++ testLigoski.s
@@ -3,13 +3,20 @@
 .include "./Funcoes/prints"
 #.include "./Funcoes/Menu"
 #.include "./Funcoes/specialEffects"
-#.include "./Funcoes/userInterface"
-#.include "./Funcoes/backend"
+.include "./Funcoes/userInterface"
+.include "./Funcoes/backend"
 
 .text
 Inicio:
-
-#	explodePlanet()
+	li s3 699		# dei um valor para teste
+	printbitmapEficiente( zero zero background)
+	alterScoreP1(  )
+	li s4 93		# dei um valor para teste
+	alterScoreP2(  )
 	
 	li a7 10
 	ecall
+
+
+N2IP1: num2img( s3 t4 )
+N2IP2: num2img( s4 t4 )
